0afd1e603843c982afdd08adb561dc459280d464
chloemk <minsung.chloe.kim@gmail.com>
2021-12-17 15:52:54 +0900

    JS Maximum Subarray

---
+++ 2021-12-16-3/index.js
@@ -0,0 +1,15 @@
+var maxSubArray = function (nums) {
+	let maxSub = nums[0];
+	let curSum = 0;
+	for (i = 0; i < nums.length; i++) {
+		// negative prefix가 되면 0으로 초기화 시킨다.
+		if (curSum < 0) {
+			curSum = 0;
+		}
+		curSum += nums[i];
+		maxSub = Math.max(maxSub, curSum);
+	}
+	return maxSub;
+};
+
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
